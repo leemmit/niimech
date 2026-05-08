c494f1dcca22df36197af94217a7bf976fdaedb7
leemmit <kolmykovl226@gmail.com>
2026-05-08 16:51:14 +0300

    fixing images deployment

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,13 @@
 import type { NextConfig } from "next";
 
+const repoName = "niimech";
+
+module.exports = {
+  output: "export",
+  basePath: `/${repoName}`,
+  assetPrefix: `/${repoName}/`,
+};
+
 const nextConfig: NextConfig = {
   /* config options here */
 };
